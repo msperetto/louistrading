676b11128fc697d621659f9b4ebb1d6d16b99dde
Lucio Toledo <luciozt@gmail.com>
2024-12-29 11:16:27 -0300

    Refactor moving DB string connection to a config file
    This way we define the DB string in a single place. Other files can then to use the same string value.
    It also make things easier when dealing with different environments. We just need to change this config file.

---
+++ main/config/config.py
@@ -0,0 +1,5 @@
+# Connection string for the database
+DEV_ENV_CON = "dbname=noshirt user=peretto"
+# DEV_ENV_CON = "dbname=noshirt user=postgres password=@tkTYB9i"
+
+# Other global settings may be added here in the future
