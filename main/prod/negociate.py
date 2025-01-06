e6cbca547f77fb0fee5fda5852b2918020d17480
Lucio Toledo <ltoledo@cormant.biz>
2025-01-05 22:29:37 -0300

    Refactor Negociate class encapsulating common logic in new methods
    Other improvements:
    - Simplified the logic when possible.
    - Rename some methods to start with "_", as this indicates is like a "private" method (since is used only by this class).

--- main/prod/negociate.py
+++ main/prod/negociate.py
@@ -13,67 +13,84 @@ class Negociate():
     
     def open_position(self, side, total_value, strategy_id):
         if NEGOCIATION_ENV == Environment_Type.TEST:
-            order_response = {}
-            cur_price = Binance().get_orderbook(self.pair, 5) 
-            order_response['orderId'] = random.randint(11111, 99999)
-            order_response['updateTime'] = datetime.now()
-            order_response['symbol'] = self.pair
-            order_response['positionSide'] = side
-            if side == "long":
-                order_response['avgPrice'] = cur_price['asks'][0][0]
-                order_response['origQty'] = total_value/float(order_response['avgPrice'])
-                order_response['status'] = 'filled'
-            else:
-                order_response['avgPrice'] = cur_price['bids'][0][0]
-                order_response['origQty'] = total_value/float(order_response['avgPrice'])
-                order_response['status'] = 'filled'
-        
+            order_response = self._simulate_test_order(side, total_value)        
         else:
             order_quantity = total_value/float(Binance().get_symbol_price(self.pair))
             order_response = Binance().open_position(self.pair, order_quantity, side, self.api_id, self.api_key)
 
-        self.register_open_transaction(order_response, strategy_id)
+        self._register_open_transaction(order_response, strategy_id)
+        return order_response
+
+    def _simulate_test_order(self, side, total_value):
+        order_response = {
+            'orderId': random.randint(11111, 99999),
+            'updateTime': datetime.now(),
+            'symbol': self.pair,
+            'positionSide': side,
+            'status': 'filled'
+        }
+        cur_price = Binance().get_orderbook(self.pair, 5)
+        if side == "long":
+            order_response['avgPrice'] = cur_price['asks'][0][0]
+        else:
+            order_response['avgPrice'] = cur_price['bids'][0][0]
+
+        order_response['origQty'] = total_value / float(order_response['avgPrice'])
         return order_response
 
     def close_position(self, side, total_value, strategy_id, trade_id):
         if NEGOCIATION_ENV == Environment_Type.TEST:
-            order_response = {}
-            order_response['orderId'] = random.randint(11111, 99999)
-            order_response['updateTime'] = datetime.now()
-            order_response['symbol'] = self.pair
-            order_response['positionSide'] = side
-            cur_price = Binance().get_orderbook(self.pair, 5) 
-            if side == "long":
-                order_response['avgPrice'] = cur_price['bids'][0][0]
-                order_response['origQty'] = db.get_order(trade_id)['quantity']
-                order_response['status'] = 'filled'
-            else:
-                order_response['avgPrice'] = cur_price['asks'][0][0]
-                order_response['origQty'] = db.get_order(trade_id)['quantity']
-                order_response['status'] = 'filled'
-
+            order_response = self._simulate_close_position(side, trade_id)
         else:
             order_response = Binance().close_position(self.pair, side, self.api_id, self.api_key)
         
-        self.register_close_transaction(order_response, strategy_id, trade_id)
+        self._register_close_transaction(order_response, strategy_id, trade_id)
         return order_response
 
+    def _simulate_close_position(self, side, trade_id):
+        order_response = {
+            'orderId': random.randint(11111, 99999),
+            'updateTime': datetime.now(),
+            'symbol': self.pair,
+            'positionSide': side,
+            'status': 'filled'
+        }
+        cur_price = Binance().get_orderbook(self.pair, 5)
+        trade_quantity = db.get_order(trade_id)['quantity']
+        if side == "long":
+            order_response['avgPrice'] = cur_price['bids'][0][0]
+        else:
+            order_response['avgPrice'] = cur_price['asks'][0][0]
 
-    def register_open_transaction(self, order_response, strategy_id):
+        order_response['origQty'] = trade_quantity
+        return order_response
 
+    def _register_open_transaction(self, order_response, strategy_id):
         trade_id = db.insert_trade_transaction(strategy_id, True, order_response)
         db.insert_order_transaction(order_response, "Entry", trade_id)
         
-    def register_close_transaction(self, order_response, strategy_id, trade_id):
-        entry_price = db.get_order(trade_id)['entry_price']
-        entry_quantity = db.get_order(trade_id)['quantity']
-        profit = (float(order_response['avgPrice'])*order_response['origQty']) - (entry_price*entry_quantity)
-        spread = (float(order_response['avgPrice']) / entry_price) - 1
-        #need to calculate roi
-        roi = 1
+    def _register_close_transaction(self, order_response, strategy_id, trade_id):
+        # TODO: Refactor to call order_dao.get_order_by_trade_id. Ideally, it should return an "order" object.
+        trade_data = db.get_order(trade_id)
+        entry_price = trade_data['entry_price']
+        entry_quantity = trade_data['quantity']
+
+        # Calculates profit, spread and ROI values
+        close_price = float(order_response['avgPrice'])
+        close_quantity = order_response['origQty']
+        profit = (close_price * close_quantity) - (entry_price * entry_quantity)
+        spread = (close_price / entry_price) - 1
+        roi = self._calculate_roi()
+
+        # Updates DB tables.
         db.update_trade_transaction(trade_id, strategy_id, order_response, profit, spread, roi)
         db.insert_order_transaction(order_response, "Close", trade_id)
         
+    # Calculates the correct ROI.    
+    def _calculate_roi(self):
+        # TODO: Figure out how to calculate the ROI. 
+        return 1
+    
     def alert_open_transaction():
         pass
 
