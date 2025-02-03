7075302274db6a027b31acf5750299bc095549f5
Marcos Peretto <mspereto@gmail.com>
2025-02-03 15:04:52 +0100

    change password in config file

--- main/config/config.py
+++ main/config/config.py
@@ -2,8 +2,7 @@ import logging
 from common.enums import Environment_Type
 
 # Connection string for the database
-# DEV_ENV_CON = "dbname=noshirt user=peretto"
-DEV_ENV_CON = "host=db dbname=noshirt user=postgres password=@tkTYB9i"
+DEV_ENV_CON = "host=db dbname=noshirt user=postgres password=postgres"
 
 # Definig environment
 NEGOCIATION_ENV =  Environment_Type.BACKTEST
