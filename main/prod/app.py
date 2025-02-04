a8435a8f8a307b221e37ae95e24b43ae3bba7e50
Marcos Peretto <mspereto@gmail.com>
2025-02-04 18:29:19 +0100

    Fix typo in strategy_B2

--- main/prod/app.py
+++ main/prod/app.py
@@ -3,7 +3,7 @@ from prod.tradingBot import TradingBot
 from common.dao import database_operations as db
 from prod.env_setup import Env_setup
 from prod.candle_data import CandleData
-from prod.released_strategies.strtategy_B2 import Strategy_B2
+from prod.released_strategies.strategy_B2 import Strategy_B2
 from prod.login import Login
 from backtesting.lib import resample_apply
 import pandas_ta as ta
@@ -12,6 +12,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+#  TODO: 
 class Main():
     def __init__(self):
         base_config = db.get_initial_config()
