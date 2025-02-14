f8276a34c4b008adfb7bb153263d75215bd1f728
Marcos Peretto <mspereto@gmail.com>
2025-02-14 18:55:37 +0100

    Add import and attributes to tune in optimization;

--- main/backtest/backtest_manager_strategy.py
+++ main/backtest/backtest_manager_strategy.py
@@ -1,13 +1,16 @@
+import importlib
+import pkgutil
+
+from config.config import STRATEGIES_PATH, STRATEGIES_MODULE
 from common.filter import *
 from common.trigger import *
 from common.trade import *
 from common.trend import *
 from backtesting import Backtest, Strategy
 from backtesting.lib import resample_apply
-from main.common.strategybuy import StrategyBuy
-from main.common.strategysell import StrategySell
-from main.common.trendanalysis import TrendAnalysis
-from main.common.strategyLong import StrategyLong
+from common.strategybuy import StrategyBuy
+from common.strategysell import StrategySell
+from common.trendanalysis import TrendAnalysis
 import pandas_ta as ta
 import pandas as pd
 from time import sleep
@@ -23,11 +26,48 @@ def import_all_strategies():
 
 # It deals with a single strategy (passed as a parameter) 
 class BacktestManagerStrategy(Strategy):
-    strategyName = ''
+    trend_ema_short = 0
+    trend_sma_medium = 0
+    trend_sma_long = 0
+    intraday_adx = 0
+    adx_trend_layer = 0
+    intraday_interval = "h"
+    trend_interval = "D"
+    intraday_sma_short = 0
+    intraday_sma_medium = 0
+    intraday_sma_long = 0
+    intraday_ema_short = 0
+    intraday_ema_medium = 0
+    intraday_ema_long = 0
+    intraday_rsi = 0
+    intraday_rsi_layer_cheap = 0
+    intraday_rsi_layer_expensive = 0
+    candles_after_triggered = 0
+    intraday_max_candles_buy = 0
+    intraday_max_candles_sell = 0
+    stop_loss = None
+    take_profit = None
+    trend_longest_indicator_value = 40
+    trend_class = "self.trend = UpTrend_AlwaysTrend()"
+    filter_buy_class = "self.filterBuy = Filter_alwaysTrue()"
+    trigger_buy_class = "self.triggerBuy = TriggeredState_alwaysTrue()"
+    trade_buy_class = "self.tradeBuy = TradeBuy_HighLastCandle(self.data)"
+    filter_sell_class = "self.filterSell = Filter_alwaysTrue()"
+    trigger_sell_class = "self.triggerSell = TriggeredState_alwaysTrue()"
+    trade_sell_class = "self.tradeSell = TradeSell_LowLastCandle(self.data)"
+    strategy_class = None
 
-    # def init(self):
-        # TODO: Define strategy class
-        # self.strategy 
+    def init(self):
+        # import_all_strategies()
+
+        # print(f"Strategy B1A: {Strategy_B1A}")
+        # print(f"strategy B2B: {Strategy_B2B}")
+
+        print(f"Strategy class is: {globals().get(self.strategy_class)}")
+        # self.position = None # Not sure if we really need this. Perhaps position is already initialized by Backtesting.Strategy
+        # Maybe set somehow all necessary classes used by the strategies parameter
+
+        self.strategy = globals().get(self.strategy_class)()
 
     def next(self):
         # maybe deal with stop loss and stop gain?
