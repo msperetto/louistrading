f25d13baffbc1ffcddbe1ad84996a9624c973ae0
Lucio Toledo <luciozt@gmail.com>
2025-02-16 12:18:20 -0300

    Add ability to run SHORT operations even when running Strategies in the BacktestManagerIntraday
    In the future, when running Strategies that should be managed by BacktestManagerStrategy (not BacktestManagerIntraday).
    However, at this stage, we still use BacktestManagerIntraday.
    
    I'm trying to analyze why the results of the backtesting for SHORT operation is not working as expected. So, I needed to create a new strategy with singles classes, and then I can set the "should_plot_chart" to true.

--- main/common/strategy.py
+++ main/common/strategy.py
@@ -88,3 +88,27 @@ class Strategy_B2():
         self.trade_sell_class = "TradeSell_Price_EMAshort"
         if (shouldIncludeTrend):
             self.trend_class = "UpTrend_Price_gt_SMAlong"
+
+class Strategy_Short_1():
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
+        if not optimize:
+            self.intraday_ema_short = 9
+            self.intraday_sma_medium = 19
+            self.intraday_sma_long = 51
+            self.trend_longest_indicator_value = 51
+        else:
+            self.intraday_ema_short = range(6,12,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = range(48,52,1)
+            self.trend_longest_indicator_value = 51
+        
+        self.intraday_interval = "2h"
+        self.trend_interval = "1d"
+        self.filter_buy_class = "Filter_alwaysTrue"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_Price_EMAshort"
+        self.filter_sell_class = "FilterSell_EMAshort_lt_SMAmedium"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_LowLastCandle"
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_Price_gt_SMAlong"
