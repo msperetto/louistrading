73929e871cfd2b45e765811957dd4e3876abe149
Marcos Peretto <mspereto@gmail.com>
2025-02-19 20:08:30 +0100

    Refactor strategyB2 to be used in the new pattern

--- main/common/strategies/strategyB2.py
+++ main/common/strategies/strategyB2.py
@@ -5,42 +5,40 @@ class Strategy_B2(StrategyLong):
     B2 strategy that configures the necessary components and indicator parameters.
     """
 
-    def __init__(self, db):
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
         super().__init__()
         # Define parameters for indicators
-        self.trend_ema_short = 6
-        self.trend_sma_medium = 18
-        self.trend_sma_long = 50
-        self.intraday_ema_short = 9
-        self.intraday_sma_medium = 19
-        self.intraday_sma_long = 51
+        if not optimize:
+            self.trend_ema_short = 6
+            self.trend_sma_medium = 18
+            self.trend_sma_long = 50
+            self.intraday_ema_short = 9
+            self.intraday_sma_medium = 19
+            self.intraday_sma_long = 51
+            self.trend_longest_indicator_value = 51
+        else:
+            self.trend_ema_short = 6
+            self.trend_sma_medium = 18
+            self.trend_sma_long = range(48,52,1)
+            self.intraday_ema_short = range(7,12,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = 51
+            self.trend_longest_indicator_value = 51
 
-        self.intraday_interval = "1h"
+
+        self.intraday_interval = "2h"
         self.trend_interval = "1d"
 
         # Define components for strategy buy logic
-        self.filter_buy_class = db.get_class_code("FilterBuy_SMAMedium_gt_SMALong")
-        self.trigger_buy_class = db.get_class_code("TriggeredStateBuy_alwaysTrue")
-        self.trade_buy_class = db.get_class_code("TradeBuy_Example")
+        self.filter_buy_class = "FilterBuy_SMAmedium_gt_SMAlong"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_HighLastCandle_EMAshort_gt_SMAmedium"
 
         # Define components for strategy sell logic
-        self.filter_sell_class = db.get_class_code("FilterSell_Example")
-        self.trigger_sell_class = db.get_class_code("TriggeredStateSell_Example")
-        self.trade_sell_class = db.get_class_code("TradeSell_Example")
-
-        # Construct the strategy buy and sell logic using the base class methods
-        self.strategy_buy = self.build_strategy_buy(
-            self.filter_buy_class,
-            self.trigger_buy_class,
-            self.trade_buy_class,
-        )
-
-        self.strategy_sell = self.build_strategy_sell(
-            self.filter_sell_class,
-            self.trigger_sell_class,
-            self.trade_sell_class,
-        )
+        self.filter_sell_class = "Filter_alwaysTrue"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_Price_EMAshort"
 
         # Define other strategy components
-        self.trend_class = db.get_class_code("UpTrend_Price_gt_SMAlong")
-        self.trend_analysis = self.build_trend_analysis(self.trend_class)
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_Price_gt_SMAlong"
