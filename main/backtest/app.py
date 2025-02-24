83edc0b68c0ba9c40e037ef3c5fb4089612b6f90
Lucio Toledo <luciozt@gmail.com>
2025-02-24 00:48:53 -0300

    Add new strategies for Short operations

--- main/backtest/app.py
+++ main/backtest/app.py
@@ -51,6 +51,9 @@ class Main():
             "ST1": Strategy_Short_Test1(optimize=self.optimize, shouldIncludeTrend=self.shouldIncludeTrend),
             "S1": Strategy_S1(optimize=self.optimize, shouldIncludeTrend=self.shouldIncludeTrend),
             "S6": Strategy_S6(optimize=self.optimize, shouldIncludeTrend=self.shouldIncludeTrend),
+            "SH1": Strategy_SH1(optimize=self.optimize, shouldIncludeTrend=self.shouldIncludeTrend),
+            "SH2": Strategy_SH2(optimize=self.optimize, shouldIncludeTrend=self.shouldIncludeTrend),
+            "SH3": Strategy_SH3(optimize=self.optimize, shouldIncludeTrend=self.shouldIncludeTrend),
         }
 
         # Inicializinzg some vars
