a61c41da651818023cfccbb43bee18dccca2d3dd
Lucio Toledo <luciozt@gmail.com>
2025-02-27 01:16:13 -0300

    Add new trade class

--- main/common/trade.py
+++ main/common/trade.py
@@ -77,6 +77,16 @@ class TradeBuy_HighLastCandle_EMAshort_gt_SMAmedium(Trade):
     def buyConfirmation(self):
         return (util.get_value_by_index(self.data.Close, -1) > util.get_value_by_index(self.data.High, -2)) and \
                 (util.get_value_by_index(self.intraday_ema_short(), -1) > util.get_value_by_index(self.intraday_sma_medium(), -1))
+    
+class TradeBuy_Close_gt_CloseLastCandle_EMAshort_gt_SMAmedium(Trade):
+    def __init__(self, data, intraday_ema_short, intraday_sma_medium):
+        self.data = data
+        self.intraday_ema_short = intraday_ema_short
+        self.intraday_sma_medium = intraday_sma_medium
+
+    def buyConfirmation(self):
+        return (util.get_value_by_index(self.data.Close, -1) > util.get_value_by_index(self.data.Close, -2)) and \
+                (util.get_value_by_index(self.intraday_ema_short(), -1) > util.get_value_by_index(self.intraday_sma_medium(), -1))
 
 class TradeSell_LowLastCandle(Trade):
     def __init__(self, data):
