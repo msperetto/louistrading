78f48f044fdab12fdbb09b42b997252002e05b9f
Lucio Toledo <luciozt@gmail.com>
2025-03-02 00:46:56 -0300

    Add new SHORT strategies

--- main/common/strategy.py
+++ main/common/strategy.py
@@ -576,7 +576,126 @@ class Strategy_SC1():
             self.intraday_sma_medium = 17
             self.intraday_sma_long = 48
             self.intraday_rsi_layer_cheap = 22
-            self.intraday_rsi_layer_expensive = 75
+            self.intraday_rsi_layer_expensive = 77
+            self.intraday_rsi = 3
+            self.trend_longest_indicator_value = 51
+        else:
+            self.trend_ema_short = range(6,11,1)
+            self.trend_sma_medium = range(18,22,1)
+            self.trend_sma_long = range(48,51,1)
+            self.intraday_ema_short = range(6,11,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = range(48,51,1)
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = range(75,81,1)
+            self.intraday_rsi = range(3,5,1)
+            self.trend_longest_indicator_value = 51
+        
+        self.intraday_interval = "2h"
+        self.trend_interval = "1d"
+
+        self.filter_sell_class = "FilterSell_RSI"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_Close_lt_CloseLastCandle"
+
+        self.filter_buy_class = "Filter_alwaysTrue"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_HighLastCandle"
+
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_Price_gt_SMAlong"
+
+
+class Strategy_SC1A():
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
+        if not optimize:
+            self.trend_ema_short = 8
+            self.trend_sma_medium = 18
+            self.trend_sma_long = 48
+            self.intraday_ema_short = 6
+            self.intraday_sma_medium = 17
+            self.intraday_sma_long = 48
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = 77
+            self.intraday_rsi = 3
+            self.trend_longest_indicator_value = 51
+        else:
+            self.trend_ema_short = range(6,11,1)
+            self.trend_sma_medium = range(18,22,1)
+            self.trend_sma_long = range(48,51,1)
+            self.intraday_ema_short = range(6,11,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = range(48,51,1)
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = range(75,81,1)
+            self.intraday_rsi = range(3,5,1)
+            self.trend_longest_indicator_value = 51
+        
+        self.intraday_interval = "2h"
+        self.trend_interval = "1d"
+
+        self.filter_sell_class = "FilterSell_RSI"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_Close_lt_CloseLastCandle"
+
+        self.filter_buy_class = "Filter_alwaysTrue"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_HighLastCandle"
+
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_Price_gt_EMAshort"
+
+
+class Strategy_SC1B():
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
+        if not optimize:
+            self.trend_ema_short = 6
+            self.trend_sma_medium = 18
+            self.trend_sma_long = 48
+            self.intraday_ema_short = 6
+            self.intraday_sma_medium = 17
+            self.intraday_sma_long = 48
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = 76
+            self.intraday_rsi = 3
+            self.trend_longest_indicator_value = 51
+        else:
+            self.trend_ema_short = range(6,11,1)
+            self.trend_sma_medium = range(18,22,1)
+            self.trend_sma_long = range(48,51,1)
+            self.intraday_ema_short = range(6,11,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = range(48,51,1)
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = range(75,81,1)
+            self.intraday_rsi = range(3,5,1)
+            self.trend_longest_indicator_value = 51
+        
+        self.intraday_interval = "2h"
+        self.trend_interval = "1d"
+
+        self.filter_sell_class = "FilterSell_RSI"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_Close_lt_CloseLastCandle"
+
+        self.filter_buy_class = "Filter_alwaysTrue"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_HighLastCandle"
+
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_EMAshort_gt_SMAmedium"
+
+class Strategy_SC1C():
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
+        if not optimize:
+            self.trend_ema_short = 6
+            self.trend_sma_medium = 21
+            self.trend_sma_long = 48
+            self.intraday_ema_short = 6
+            self.intraday_sma_medium = 17
+            self.intraday_sma_long = 48
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = 79
             self.intraday_rsi = 4
             self.trend_longest_indicator_value = 51
         else:
@@ -603,7 +722,7 @@ class Strategy_SC1():
         self.trade_buy_class = "TradeBuy_HighLastCandle"
 
         if (shouldIncludeTrend):
-            self.trend_class = "DownTrend_SMAmedium_lt_SMAlong"
+            self.trend_class = "UpTrend_SMAmedium_gt_SMAlong"
 
 class Strategy_SC2():
     def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
@@ -799,3 +918,120 @@ class Strategy_SC6():
 
         if (shouldIncludeTrend):
             self.trend_class = "DownTrend_SMAmedium_lt_SMAlong"
+
+class Strategy_SC6A():
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
+        if not optimize:
+            self.trend_ema_short = 8
+            self.trend_sma_medium = 18
+            self.trend_sma_long = 48
+            self.intraday_ema_short = 6
+            self.intraday_sma_medium = 21
+            self.intraday_sma_long = 50
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = 79
+            self.intraday_rsi = 3
+            self.trend_longest_indicator_value = 51
+        else:
+            self.trend_ema_short = range(6,11,1)
+            self.trend_sma_medium = range(18,22,1)
+            self.trend_sma_long = range(48,51,1)
+            self.intraday_ema_short = range(6,11,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = range(48,51,1)
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = range(75,81,1)
+            self.intraday_rsi = range(3,5,1)
+            self.trend_longest_indicator_value = 51
+        
+        self.intraday_interval = "2h"
+        self.trend_interval = "1d"
+
+        self.filter_sell_class = "FilterSell_RSI"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_Low_lt_LowLastCandle"
+
+        self.filter_buy_class = "Filter_alwaysTrue"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_HighLastCandle_EMAshort_gt_SMAmedium"
+
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_EMAshort_gt_SMAmedium"
+
+class Strategy_SC6B():
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
+        if not optimize:
+            self.trend_ema_short = 6
+            self.trend_sma_medium = 18
+            self.trend_sma_long = 48
+            self.intraday_ema_short = 6
+            self.intraday_sma_medium = 21
+            self.intraday_sma_long = 50
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = 75
+            self.intraday_rsi = 4
+            self.trend_longest_indicator_value = 51
+        else:
+            self.trend_ema_short = range(6,11,1)
+            self.trend_sma_medium = range(18,22,1)
+            self.trend_sma_long = range(48,51,1)
+            self.intraday_ema_short = range(6,11,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = range(48,51,1)
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = range(75,81,1)
+            self.intraday_rsi = range(3,5,1)
+            self.trend_longest_indicator_value = 51
+        
+        self.intraday_interval = "2h"
+        self.trend_interval = "1d"
+
+        self.filter_sell_class = "FilterSell_RSI"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_Low_lt_LowLastCandle"
+
+        self.filter_buy_class = "Filter_alwaysTrue"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_HighLastCandle_EMAshort_gt_SMAmedium"
+
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_Price_gt_EMAshort"
+
+class Strategy_SC6C():
+    def __init__(self, optimize: bool = False, shouldIncludeTrend: bool = False):
+        if not optimize:
+            self.trend_ema_short = 6
+            self.trend_sma_medium = 21
+            self.trend_sma_long = 50
+            self.intraday_ema_short = 6
+            self.intraday_sma_medium = 18
+            self.intraday_sma_long = 48
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = 78
+            self.intraday_rsi = 4
+            self.trend_longest_indicator_value = 51
+        else:
+            self.trend_ema_short = range(6,11,1)
+            self.trend_sma_medium = range(18,22,1)
+            self.trend_sma_long = range(48,51,1)
+            self.intraday_ema_short = range(6,11,1)
+            self.intraday_sma_medium = range(18,22,1)
+            self.intraday_sma_long = range(48,51,1)
+            self.intraday_rsi_layer_cheap = 22
+            self.intraday_rsi_layer_expensive = range(75,81,1)
+            self.intraday_rsi = range(3,5,1)
+            self.trend_longest_indicator_value = 51
+        
+        self.intraday_interval = "2h"
+        self.trend_interval = "1d"
+
+        self.filter_sell_class = "FilterSell_RSI"
+        self.trigger_sell_class = "TriggeredState_alwaysTrue"
+        self.trade_sell_class = "TradeSell_Low_lt_LowLastCandle"
+
+        self.filter_buy_class = "Filter_alwaysTrue"
+        self.trigger_buy_class = "TriggeredState_alwaysTrue"
+        self.trade_buy_class = "TradeBuy_HighLastCandle_EMAshort_gt_SMAmedium"
+
+        if (shouldIncludeTrend):
+            self.trend_class = "UpTrend_SMAmedium_gt_SMAlong"
