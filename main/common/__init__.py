01f5fcf872623c3741a00527a68c58d9f27b7ec4
Marcos Peretto <mspereto@gmail.com>
2025-03-05 16:12:01 +0100

    Refactor to include automatically strategies import on Prod

--- main/common/__init__.py
+++ main/common/__init__.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 
-# Strategies Folder:
-STRATEGIES_MODULE = "common.strategies"
+# Backtest Strategies Folder:
+STRATEGIES_MODULE_BT = "common.strategies"
 
-# Strategies Path:
-STRATEGIES_PATH = Path(__file__).parent.parent / 'common' / 'strategies'
+# Backtest Strategies Path:
+STRATEGIES_PATH_BT = Path(__file__).parent.parent / 'common' / 'strategies'
+
+# Prod Strategies Folder:
+STRATEGIES_MODULE_PROD = "prod.released_strategies"
+
+# Prod Strategies Path:
+STRATEGIES_PATH_PROD = Path(__file__).parent.parent / 'prod' / 'released_strategies'
