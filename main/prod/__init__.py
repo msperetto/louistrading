bd70d18a72eb6d7afb39636899dd1c129c431a56
Marcos Peretto <mspereto@gmail.com>
2025-03-06 17:11:05 +0100

    Fix before assigning paths to variables, check if exists & create if not

--- main/prod/__init__.py
+++ main/prod/__init__.py
@@ -3,7 +3,15 @@ import logging
 from config.config import LOG_LEVEL
 
 # Configure logging
-log_file_path = os.path.join(os.path.dirname(__file__), 'logs', 'trading_bot.log')
+log_dir = os.path.join(os.path.dirname(__file__), 'logs')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+log_file_path = os.path.join(log_dir, 'trading_bot.log')
+if not os.path.exists(log_file_path):
+    with open(log_file_path, 'w'):
+        pass
+
 logging.basicConfig(
     filename=log_file_path,
     filemode='a',
