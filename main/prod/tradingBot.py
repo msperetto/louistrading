msperetto/louistrading

commit befff2eae730593183a0b54e65fb4c31b78397fe

Add pair precision calculation for open positions
Each pair has a different max decimal places, so we need to adjust that to open the orders correctly;

main/prod/tradingBot.py
@@ -24,6 +24,7 @@ import time
 from config.config import NEGOCIATION_ENV
 from common.enums import Environment_Type
 from prod import logger
+from common.util import get_side, get_pairs_precision
 
 
 class TradingBot:
@@ -46,6 +47,9 @@ class TradingBot:
         # setting binance initial leverage value
         for pair in self.db.get_active_pairs():
             self._set_leverage(pair, self.setup.leverage_long_value)
+
+        # getting decimal precision by pair:
+        self.pairs_precision = get_pairs_precision(self.db.get_active_pairs())
 
     def stop(self):
         logger.info("Stopping bot...")
@@ -168,6 +172,7 @@ class TradingBot:
 
                 manager = StrategyManager(
                     pair,
+                    self.pairs_precision[pair],
                     final_dataset,
                     self.exchange_session.e_id,
                     self.exchange_session.e_sk,

main/common/util.py
@@ -6,7 +6,8 @@ import importlib
 import pkgutil
 import os
 from prod import logger, telegram_logger, aws_logger
-
+from typing import List, Dict
+from prod.binance import Binance
 
 def get_value_by_index(series, index):
     """
@@ -52,4 +53,24 @@ def get_server_public_ip(server_ip_name):
         return None
     except Exception as e:
         aws_logger.error(f"Error retrieving public IP: {e}")
+        return None
+
+def get_pairs_precision(pairs: List) -> Dict:
+    """
+    Returns the precision of the given list of pairs.
+    So when passing the quantity of an order to the exchange, the decimal places are correct.
+    """
+    precision_result = {}
+    try:
+        exchange_info = Binance().get_exchange_info()
+        for pair in pairs:
+            pair_result = {pair: symbol['quantityPrecision'] for symbol in exchange_info['symbols'] if symbol['symbol'] == pair}
+            if pair_result:
+                precision_result.update(pair_result)
+            else:
+                logger.warning(f"Pair {pair} not found in exchange info.")
+        return precision_result
+
+    except Exception as e:
+        logger.error(f"Error retrieving precision for {pair}: {e}")
         return None

main/prod/negotiate.py
@@ -4,13 +4,14 @@ from common.dao import database_operations as db
 import random
 from datetime import datetime
 from config.config import NEGOCIATION_ENV
-from common.enums import Environment_Type
+from common.enums import Environment_Type, Side_Type
 from prod import logger
 
 
 class Negotiate():
-    def __init__(self, pair, api_id, api_key):
+    def __init__(self, pair, pair_precision, api_id, api_key):
         self.pair = pair
+        self.pair_precision = pair_precision
         self.api_id = api_id
         self.api_key = api_key
     
@@ -20,7 +21,7 @@ class Negotiate():
         if NEGOCIATION_ENV == Environment_Type.TEST:
             order_response = self._simulate_test_order(side, total_value)        
         else:
-            order_quantity = total_value/float(Binance().get_symbol_price(self.pair))
+            order_quantity = round(total_value/float(Binance().get_symbol_price(self.pair)), self.pair_precision)
             order_response = Binance().open_position(self.pair, order_quantity, side, self.api_id, self.api_key)
 
         if order_response is None:
@@ -115,7 +116,3 @@ class Negotiate():
 
     def alert_close_transaction():
         pass
-
-# negoc = Negotiate("XTZUSDT", "a", "b")
-# negoc.open_position(Side_Type.LONG, 7200, 1)
-# negoc.close_position(Side_Type.LONG, 7200, 1)

main/prod/strategy_manager.py
@@ -23,8 +23,9 @@ from common.indicators_catalog import indicators_catalog
 class StrategyManager():
 
     #modificar aqui para ser somente uma estratégia
-    def __init__(self, pair, dataset, api_id, api_key, order_value, strategy, stop_loss = None, take_profit = None):
+    def __init__(self, pair, pair_precision, dataset, api_id, api_key, order_value, strategy, stop_loss = None, take_profit = None):
         self.pair = pair
+        self.pair_precision = pair_precision
         self.data = dataset
         self.api_id = api_id
         self.api_key = api_key
@@ -32,7 +33,7 @@ class StrategyManager():
         self.strategy = strategy
         self.stop_loss = stop_loss
         self.take_profit = take_profit
-        self.negotiate = Negotiate(self.pair, self.api_id, self.api_key)
+        self.negotiate = Negotiate(self.pair, self.pair_precision, self.api_id, self.api_key)
         self.classes = {}
         self.set_support_objects()
 

commit 7b9c90be2b7fce9729a17e25bba6423f4a112a77

Fix pair precision calculation for opened orders
The argument was not being passed in the method to evaluate already opened orders;

Error introduced by commit: befff2eae730593183a0b54e65fb4c31b78397fe

Fix pair parameter, using trade.pair instead

main/prod/tradingBot.py
@@ -220,6 +220,7 @@ class TradingBot:
 
             manager = StrategyManager(
                 trade.pair,
+                self.pairs_precision[trade.pair],
                 final_dataset,
                 self.exchange_session.e_id,
                 self.exchange_session.e_sk,