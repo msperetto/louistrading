5fac850d0b24adee5408361c09a21b9a0e5b6a83
Marcos Peretto <mspereto@gmail.com>
2025-05-06 18:19:06 +0200

    Fix returning account balance as object instead of a list

--- main/common/dao/account_balance_dao.py
+++ main/common/dao/account_balance_dao.py
@@ -37,12 +37,10 @@ def get_account_balance(account_id):
             """, (account_id,))
             row = cur.fetchone()
             if row:
-                return [
-                    AccountBalance(
-                        account_id=row["account_id"],
-                        account_balance=row["usdt_balance"],
-                        margin_ratio=row["margin_ratio"],
-                        date_updated=row["last_update"]
-                    )
-                ]            
+                return AccountBalance(
+                    account_id=row["account_id"],
+                    account_balance=row["usdt_balance"],
+                    margin_ratio=row["margin_ratio"],
+                    date_updated=row["last_update"]
+                )
             return None
