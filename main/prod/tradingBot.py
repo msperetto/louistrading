6131b621b3a45c68d1f8c0e82d04b2adc67ee212
Marcos Peretto <mspereto@gmail.com>
2025-05-13 19:36:00 +0200

    Refactor to each loop deal with specific errors,not blocking other coins
    Created try/catch for handle_new_coins and handle_opened_trades, so if one coin has some problem, it doesn't block other coins;

--- main/prod/tradingBot.py
+++ main/prod/tradingBot.py
@@ -86,7 +86,7 @@ class TradingBot:
                     time.sleep(20)
             except Exception as e:
                 notify.send_message_alert(
-                    f"An error occurred in the bot: \n {e}. \n Please check the logs for more details."
+                    f"An error occurred in the bot main loop(run): \n {e}. \n Please check the logs for more details."
                 )
                 logger.error(f"An error occurred: {e}")
                 logger.info("Retrying in 2 minutes...")
@@ -155,47 +155,55 @@ class TradingBot:
 
                 # TODO: Checar "saldo" (talvez collateral) numa tabela interna?.
                 # Prevenir novas operações caso não tenha saldo suficiente.
-                
-                # Check if the strategy has been executed recently
-                if not self.should_run_strategy(pair, strategy):
-                    # Jump to the next strategy for this pair.
-                    continue
+
+                try:
                     
-                logger.debug(f"handle_new_trades - pair: {pair} - strategy: {strategy}")
-                logger.debug(f"current balance: {self.current_balance}")
+                    # Check if the strategy has been executed recently
+                    if not self.should_run_strategy(pair, strategy):
+                        # Jump to the next strategy for this pair.
+                        continue
+                        
+                    logger.debug(f"handle_new_trades - pair: {pair} - strategy: {strategy}")
+                    logger.debug(f"current balance: {self.current_balance}")
 
-                #Check balance; Stop new trades if balance is below the minimum required.
-                if self._is_balance_below_minimum():
-                    logger.info(f"Current balance is below the minimum required. Current balance: {self.current_balance}")
-                    return
+                    #Check balance; Stop new trades if balance is below the minimum required.
+                    if self._is_balance_below_minimum():
+                        logger.info(f"Current balance is below the minimum required. Current balance: {self.current_balance}")
+                        return
 
-                # Updates the last run time
-                self.last_executions[(pair, strategy)] = datetime.now()
+                    # Updates the last run time
+                    self.last_executions[(pair, strategy)] = datetime.now()
 
-                if NEGOCIATION_ENV == Environment_Type.TEST:
-                    final_dataset = self.create_combined_dataset(pair, strategy)
-                    final_dataset = TestNegociationMain().add_fake_row(final_dataset)
-                else:
-                    final_dataset = self.create_combined_dataset(pair, strategy)
+                    if NEGOCIATION_ENV == Environment_Type.TEST:
+                        final_dataset = self.create_combined_dataset(pair, strategy)
+                        final_dataset = TestNegociationMain().add_fake_row(final_dataset)
+                    else:
+                        final_dataset = self.create_combined_dataset(pair, strategy)
 
-                #logging for debugging
-                logger.info(f"TRYING TO OPEN POSITION - Pair: {pair}")
-                logger.debug(f"Pair: {pair} - datetime: {datetime.now()} - final_dataset:\n{final_dataset}")
+                    #logging for debugging
+                    logger.info(f"TRYING TO OPEN POSITION - Pair: {pair}")
+                    logger.debug(f"Pair: {pair} - datetime: {datetime.now()} - final_dataset:\n{final_dataset}")
 
-                manager = StrategyManager(
-                    pair,
-                    self.pairs_precision[pair],
-                    final_dataset,
-                    self.exchange_session.e_id,
-                    self.exchange_session.e_sk,
-                    self.setup.order_value,
-                    strategy
-                )
-                if manager.try_open_position():
-                    available_orders -= 1
-                    self.current_balance = self._get_current_balance()
-                    # If the position was opened, then jump to the next pair.
-                    break
+                    manager = StrategyManager(
+                        pair,
+                        self.pairs_precision[pair],
+                        final_dataset,
+                        self.exchange_session.e_id,
+                        self.exchange_session.e_sk,
+                        self.setup.order_value,
+                        strategy
+                    )
+                    if manager.try_open_position():
+                        available_orders -= 1
+                        self.current_balance = self._get_current_balance()
+                        # If the position was opened, then jump to the next pair.
+                        break
+                except Exception as e:
+                    logger.error(f"An error occurred while trying to open position for pair {pair} with strategy {strategy}: {e}")
+                    notify.send_message_alert(
+                        f"An error occurred while trying to open position for pair {pair} with strategy {strategy}: {e}"
+                    )
+                    continue
 
 
     def handle_opened_trades(self):
@@ -207,41 +215,47 @@ class TradingBot:
         # TODO: What if an exception occurs in a specific trade?
         # We might want to continue processing the for loop and try to close the next trade. 
         for trade in opened_trades:
-            strategyObject = strategy_dao.get_strategy_by_id(trade.strategy_id)
-            strategyClassName = globals().get(strategyObject.name)
-            #instantiate the strategy class:
-            strategy = strategyClassName()
+            try:
+                strategyObject = strategy_dao.get_strategy_by_id(trade.strategy_id)
+                strategyClassName = globals().get(strategyObject.name)
+                #instantiate the strategy class:
+                strategy = strategyClassName()
 
-            # Check if the strategy has been opened in the last candle
-            if not self.should_run_strategy(trade.pair, strategy):
-                # Jump to the next opened trade.
+                # Check if the strategy has been opened in the last candle
+                if not self.should_run_strategy(trade.pair, strategy):
+                    # Jump to the next opened trade.
+                    continue
+
+                logger.debug(f"handle_opened_trades - pair: {trade.pair}")
+                logger.debug(f"current balance: {self.current_balance}")
+
+                if NEGOCIATION_ENV == Environment_Type.TEST:
+                    final_dataset = self.create_combined_dataset(trade.pair, strategy)
+                    final_dataset = TestNegociationMain().add_fake_row(final_dataset)
+                else:
+                    final_dataset = self.create_combined_dataset(trade.pair, strategy)
+
+                #logging for debugging
+                logger.info(f"TRYING TO CLOSE POSITION - Pair: {trade.pair}")
+                logger.debug(f"Pair: {trade.pair} - datetime: {datetime.now()} - final_dataset:\n{final_dataset}")
+
+                manager = StrategyManager(
+                    trade.pair,
+                    self.pairs_precision[trade.pair],
+                    final_dataset,
+                    self.exchange_session.e_id,
+                    self.exchange_session.e_sk,
+                    self.setup.order_value,
+                    strategy
+                )
+                if manager.try_close_position(strategy, trade.id):
+                    self.current_balance = self._get_current_balance()
+            except Exception as e:
+                logger.error(f"An error occurred while trying to close position for trade {trade.id}: {e}")
+                notify.send_message_alert(
+                    f"An error occurred while trying to close position for trade {trade.id}: {e}"
+                )
                 continue
-
-            logger.debug(f"handle_opened_trades - pair: {trade.pair}")
-            logger.debug(f"current balance: {self.current_balance}")
-
-            if NEGOCIATION_ENV == Environment_Type.TEST:
-                final_dataset = self.create_combined_dataset(trade.pair, strategy)
-                final_dataset = TestNegociationMain().add_fake_row(final_dataset)
-            else:
-                final_dataset = self.create_combined_dataset(trade.pair, strategy)
-
-            #logging for debugging
-            logger.info(f"TRYING TO CLOSE POSITION - Pair: {trade.pair}")
-            logger.debug(f"Pair: {trade.pair} - datetime: {datetime.now()} - final_dataset:\n{final_dataset}")
-
-            manager = StrategyManager(
-                trade.pair,
-                self.pairs_precision[trade.pair],
-                final_dataset,
-                self.exchange_session.e_id,
-                self.exchange_session.e_sk,
-                self.setup.order_value,
-                strategy
-            )
-            if manager.try_close_position(strategy, trade.id):
-                self.current_balance = self._get_current_balance()
-
 
 
     def should_run_strategy(self, pair, strategy):
