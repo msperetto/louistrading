768c9d41377b3d23582ac4a8fbfaabd5f0fc0166
Marcos Peretto <mspereto@gmail.com>
2025-07-02 18:05:17 +0200

    Add debug message to log, to check result of closed orders

--- main/marlinStop/stopLogic.py
+++ main/marlinStop/stopLogic.py
@@ -117,6 +117,7 @@ class StopManager():
         try:
             closed_orders = Binance().query_account_trade_list(
                 symbol=self.pair, startTime=startTime, b_id=self.api_id, b_sk=self.api_key)
+            marlin_stop_logger.debug(f"Closed orders for {symbol}: {closed_orders}")
             return closed_orders
         except Exception as e:
             marlin_stop_logger.error(f"Error checking closed stop orders: {e}")
