7b5aacbf8ef1edbb8f81e411d707ad250197abf7
Marcos Peretto <mspereto@gmail.com>
2025-07-03 17:12:39 +0200

    temp add test for trade list binance endpoint

--- main/prod/app.py
+++ main/prod/app.py
@@ -7,6 +7,8 @@ from prod.released_strategies.strategy_B2 import Strategy_B2
 from prod.login import Login
 import logging
 from prod import logger
+from prod.binance import Binance
+from time import sleep
 
 
 #  TODO: 
@@ -15,10 +17,22 @@ class Main():
         # Import all strategies from the released strategies folder.
         import_all_strategies(STRATEGIES_PATH_PROD, STRATEGIES_MODULE_PROD, globals())
 
+
         base_config = db.get_initial_config()
         self.setup = Env_setup(base_config)
         self.exchange_session = Login("binance")
         self.exchange_session.login_database()
+        
+
+        check_last_trades = Binance().query_account_trade_list(
+            symbol='XRPUSDT', 
+            startTime=1751550880232, 
+            b_id=self.exchange_session.e_id, 
+            b_sk=self.exchange_session.e_sk
+        )
+        logger.info(f"Check last trades: {check_last_trades}")
+        sleep(10)
+
         self.strategies = [Strategy_B2(), Strategy_SH7()]
 
         self.bot = TradingBot(self.strategies, db, self.setup, self.exchange_session)
