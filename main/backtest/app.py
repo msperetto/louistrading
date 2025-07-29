0e1efd103458e9a63e99c000525feab3b3a027fb
Marcos Peretto <mspereto@gmail.com>
2025-07-29 15:45:09 -0300

    Refactor passing method param as variable instead of fuction call

--- main/backtest/app.py
+++ main/backtest/app.py
@@ -313,7 +313,8 @@ class Main():
         # Iterate through each date range and run the backtest.
         # This will allow us to run the backtest for each month or custom days as defined in the config.
         for range_start, range_end in date_ranges:
-            extended_start_time = management.calc_start_date(trend_period=self.trend_interval, longest_indicator=self._get_longest_trend_indicator_from_params(), base_start_date=range_start)
+            longest_indicator = self._get_longest_trend_indicator_from_params()
+            extended_start_time = management.calc_start_date(trend_period=self.trend_interval, longest_indicator=longest_indicator, base_start_date=range_start)
             print(f"Running backtest from {range_start.date()} to {range_end.date()}")
             self.run_backtest_for_period(extended_start_time, range_end)    
 
